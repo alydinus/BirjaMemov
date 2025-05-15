9261fabb2bf16a3eda013e03bf8364fd38e471e7
alydinus <alykulovalydin@gmail.com>
2025-05-15 16:08:53 +0600

    add exception handler

---
+++ src/main/java/kg/alatoo/labor_exchange/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package kg.alatoo.labor_exchange.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
